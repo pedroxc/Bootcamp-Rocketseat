a25c9e1b76ab306e6c587474d350b984d8e215cc
Pedro Coelho <pedroxaviercoelho@gmail.com>
2021-06-03 16:26:52 -0300

    x

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Conteiner, Form, SubmitButton } from './styles';
 
-import { Title } from './styles';
-
-const name = 'Pedro';
 function Main() {
-  return <Title error>Olá {name}</Title>;
+  return (
+    <Conteiner>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Conteiner>
+  );
 }
 
 export default Main;
